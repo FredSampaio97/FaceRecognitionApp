7465fea5f1c640572aeccc3dc2dc74b7bf9b89b0
unknown <fred.skp2@gmail.com>
2024-07-15 18:27:30 +0100

    Updanting database Info

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ dotenv.config();
 const db = knex({
     client: 'pg',
     connection: {
+      connectionString : process.env.DB_URL,
+      ssl: {rejectUnauthorized: false},
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
