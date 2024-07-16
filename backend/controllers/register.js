be8b4b541decca415d20a86f10bce0cd5cfad06b
unknown <fred.skp2@gmail.com>
2024-07-16 02:39:44 +0100

    Dabase Testing

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -1,15 +1,16 @@
 export const handleRegister = (req, res, db, bcrypt) => {
     const {email, name, password} = req.body;
-    if(!email || !name || !password) {
+    if (!email || !name || !password) {
         return res.status(400).json('incorrect form submission!');
     }
-    
+
     bcrypt.hash(password, null, null, function(err, hash) {
         if (err) {
+            console.error('Error hashing password:', err);
             return res.status(500).json('Error hashing password');
         }
 
-        console.log(hash);
+        console.log('Password hash:', hash);
 
         db.transaction(trx => {
             trx.insert({
@@ -19,21 +20,25 @@ export const handleRegister = (req, res, db, bcrypt) => {
             .into('login')
             .returning('email')
             .then(loginEmail => {
+                console.log('Login email:', loginEmail);
                 return trx('users')
                 .returning('*')
                 .insert({
-                    email: loginEmail[0].email,
+                    email: loginEmail[0],
                     name: name,
                     joined: new Date()
                 })
                 .then(user => {
+                    console.log('Registered user:', user);
                     res.json(user[0]);
                 })
             })
             .then(trx.commit)
             .catch(trx.rollback);
         })
-        .catch(err => res.status(400).json('Unable to register!')); //Em caso de erro    
-    }); // Password hash creation
-} 
-
+        .catch(err => {
+            console.error('Transaction error:', err);
+            res.status(400).json('Unable to register!');
+        });
+    });
+};
