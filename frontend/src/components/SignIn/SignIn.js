095f3e922efd7ac3dd624f43c7448146f744e1db
unknown <fred.skp2@gmail.com>
2024-07-22 18:29:06 +0100

    frontend deployment

--- frontend/src/components/SignIn/SignIn.js
+++ frontend/src/components/SignIn/SignIn.js
@@ -1,67 +1,70 @@
 
 import './SignIn.css';
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 
 const SignIn = ({onRouteChange, loadUser}) => {
-    
     const [signInEmail, setEmail] = useState('');
     const [signInPassword, setPassword] = useState('');
-    
 
     const onEmailChange = (event) => {
         setEmail(event.target.value);
     }
-    
+
     const onPasswordChange = (event) => {
         setPassword(event.target.value);
     }
 
     const onSubmitSignIn = () => {
         fetch('https://facerecognitionapp-tv2a.onrender.com/signin', {
-            method:'post',
+            method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 email: signInEmail,
                 password: signInPassword
             })
         })
-            .then(response => response.json())
-            .then(user => {
-                if (user.id){ //Does the user exist? did we receive an user id?
-                    loadUser(user);
-                    onRouteChange('home');
-                }
-            })
-        
+        .then(response => {
+            console.log('Full response:', response);
+            return response.json();
+        })
+        .then(user => {
+            console.log('User:', user);
+            if (user.id) {
+                loadUser(user);
+                onRouteChange('home');
+            } else {
+                console.error('Failed to sign in:', user);
+            }
+        })
+        .catch(err => console.error('Error signing in, this is the error:', err)); //Erro
     }
 
-    return(
+    return (
         <article className="signin-box shadow-2 center">
             <main className="black-80 center">
                 <div className="measure sign-in-form">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                    <legend className="f2 fw5 ph0 mh0">Sign In</legend>
-                    <div className="mt3">
-                        <label className="db fw5 lh-copy f4" htmlFor="email-address">Email</label>
-                        <input 
-                            className="pa2 input-reset ba bg-transparent w-100" 
-                            type="email" 
-                            name="email-address"  
-                            id="email-address"
-                            onChange={onEmailChange}
+                        <legend className="f2 fw5 ph0 mh0">Sign In</legend>
+                        <div className="mt3">
+                            <label className="db fw5 lh-copy f4" htmlFor="email-address">Email</label>
+                            <input
+                                className="pa2 input-reset ba bg-transparent w-100"
+                                type="email"
+                                name="email-address"
+                                id="email-address"
+                                onChange={onEmailChange}
                             />
-                            
-                    </div>
-                    <div className="mv3">
-                        <label className="db fw5 lh-copy f4" htmlFor="password">Password</label>
-                        <input 
-                            className="b pa2 input-reset ba bg-transparent w-100" 
-                            type="password" 
-                            name="password"  
-                            id="password"
-                            onChange={onPasswordChange}   
-                        />
-                    </div>
+                        </div>
+                        <div className="mv3">
+                            <label className="db fw5 lh-copy f4" htmlFor="password">Password</label>
+                            <input
+                                className="b pa2 input-reset ba bg-transparent w-100"
+                                type="password"
+                                name="password"
+                                id="password"
+                                onChange={onPasswordChange}
+                            />
+                        </div>
                     </fieldset>
                     <div className="">
                         <input onClick={onSubmitSignIn} className="b ph5 pv2 input-reset ba b--black bg-transparent grow pointer f4 dib" type="submit" value="Sign in"/>
@@ -71,9 +74,8 @@ const SignIn = ({onRouteChange, loadUser}) => {
                     </div>
                 </div>
             </main>
-        </article>   
+        </article>
     );
-         
 }
 
 export default SignIn;
