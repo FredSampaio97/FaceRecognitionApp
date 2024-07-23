cfa2445f1277c966c8d5119a48f815939f4ec51d
unknown <fred.skp2@gmail.com>
2024-07-23 17:28:32 +0100

    Css Fixes 10

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -6,14 +6,14 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
     if(isSignedIn){
         return(
             <nav style= {{display: 'flex', justifyContent: 'flex-end'}}>
-                <p onClick={() => onRouteChange('signout')} className='f2 ph2 link dim black underline  pointer'> Sign Out </p>
+                <p onClick={() => onRouteChange('signout')} className='f3 ph2 link dim black underline  pointer'> Sign Out </p>
             </nav>
         );
     }else {
         return(
             <nav style= {{display: 'flex', justifyContent: 'flex-end'}}>
                 <p onClick={() => onRouteChange('signin')} className='f3 ph2 link dim black underline  pointer'> Sign In </p>
-                <p onClick={() => onRouteChange('register')} className='f2 ph2 link dim black underline  pointer'> Register </p>
+                <p onClick={() => onRouteChange('register')} className='f3 ph2 link dim black underline  pointer'> Register </p>
             </nav>
         );
     }
